127e7719ef1c31acf41f0447d27d7ae612ab4077
hishamrehab <hishamrehab30@gmail.com>
2025-10-08 05:19:12 +0300

    Side Effects useEffect Data Fetching with TypeScript

---
+++ 5-side effects useEffect Data Fetching with TypeScript/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import Button from './UI/Button.tsx';
+import { TimersContext, useTimersContext } from '../store/timers-centext.tsx';
+
+export default function Header() {
+ const timersCtx = useTimersContext();
+
+  return (
+    <header>
+      <h1>ReactTimer</h1>
+
+      <Button onClick={timersCtx.isRunning ? timersCtx.stopTimers : timersCtx.startTimers}>
+       {timersCtx.isRunning ? "Stop" : "Start"} Timers
+      </Button>
+    </header>
+  );
+}
