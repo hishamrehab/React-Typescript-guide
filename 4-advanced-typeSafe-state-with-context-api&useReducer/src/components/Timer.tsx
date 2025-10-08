73608485f91a80a4e2a5aca166e04ef1b30d72f8
hishamrehab <hishamrehab30@gmail.com>
2025-10-08 05:15:50 +0300

    Side Effects useEffect Data Fetching with TypeScript

--- 4-advanced-typeSafe-state-with-context-api&useReducer/src/components/Timer.tsx
+++ 4-advanced-typeSafe-state-with-context-api&useReducer/src/components/Timer.tsx
@@ -1,14 +1,30 @@
 import Container from './UI/Container.tsx';
 import { type Timer as TimerProps } from '../store/timers-centext.tsx';
-
-
+import { useEffect, useState } from 'react';
 
 
 export default function Timer({name , duration} : TimerProps) {
+ const [remainingTime , setRemainingTime ] =  useState(duration * 1000);
+  
+ useEffect(() => {
+    setInterval(() => {
+     setRemainingTime((prevTime) => prevTime - 50);
+  }, 50);
+ },[]);
+
+ const fromattedReminingTime = (remainingTime / 1000).toFixed(2);
+
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+       <progress 
+        max={duration * 1000}
+        value={remainingTime}/> 
+      </p>
+     <p>{fromattedReminingTime}</p>
+    
     </Container>
   );
 }
